8f50716f9317b9e35c7aaf1e41443053aa6ea4fa
root <xion_y@hotmail.fr>
2016-01-29 08:07:20 +0100

    Quelque modification au niveau des variables

--- MakeGateway.sh
+++ MakeGateway.sh
@@ -8,29 +8,29 @@ readonly SYSCTL_PATH=/etc/sysctl.conf
 
 asksure() {
 echo -n "(Y/N)? "
-while read -r -n 1 -s $REPLY; do
-  if [[ $REPLY = [YyNn] ]]; then
-    [[ $REPLY = [Yy] ]] && retval=0
-    [[ $REPLY = [Nn] ]] && retval=1
+while read -n 1 -s ${REPLY}; do
+  if [[ ${REPLY} = [YyNn] ]]; then
+    [[ ${REPLY} = [Yy] ]] && retval=0
+    [[ ${REPLY} = [Nn] ]] && retval=1
     break
   fi
 done
 
-return $retval
+return ${retval}
 }
 
 
 
-# check if $SYSCTL_PATH exist and ask for a new
-if [ ! -f $SYSCTL_PATH ]
+# check if ${SYSCTL_PATH} exist and ask for a new
+if [ ! -f ${SYSCTL_PATH} ]
 then
-  echo -e $SYSCTL_PATH whas not found, should i create for you ?
+  echo -e ${SYSCTL_PATH} whas not found, should i create for you ?
 
   if asksure; then
-    echo -e "\nOkay, performing echo 'net.ipv4.ip_forward = 1' >> $SYSCTL_PATH..."
-    /bin/su -c "echo 'net.ipv4.ip_forward = 1' >> $SYSCTL_PATH"
-    echo "Okay, performing sysctl -p $SYSCTL_PATH..."
-    /bin/su -c "sysctl -p $SYSCTL_PATH"
+    echo -e "\nOkay, performing echo 'net.ipv4.ip_forward = 1' >> ${SYSCTL_PATH}..."
+    /bin/su -c "echo 'net.ipv4.ip_forward = 1' >> ${SYSCTL_PATH}"
+    echo "Okay, performing sysctl -p ${SYSCTL_PATH}..."
+    /bin/su -c "sysctl -p ${SYSCTL_PATH}"
 else
   echo "Pfff..."
   exit 0
@@ -42,11 +42,11 @@ echo -e "##SELECTION DES INTERFACE##\n"
 read -p "First Interface(from) : " Interface1
 read -p "Second Interface(to) : " Interface2
 
-echo -e "\nOkay, performing iptables -t nat -A POSTROUTING --out-interface $Interfacer2 -j MASQUERADE"
+echo -e "\nOkay, performing iptables -t nat -A POSTROUTING --out-interface ${Interfacer2} -j MASQUERADE"
 
-  /sbin/iptables -t nat -A POSTROUTING --out-interface $Interface2 -j MASQUERADE
+  /sbin/iptables -t nat -A POSTROUTING --out-interface ${Interface2} -j MASQUERADE
 
-echo -e "\nOkay, performing iptables -t nat -A FORWARD --in-interface $Interface1 -j MASQUERADE" 
+echo -e "\nOkay, performing iptables -t nat -A FORWARD --in-interface ${Interface1} -j MASQUERADE" 
 
-  /sbin/iptables -A FORWARD --in-interface $Interface1 -j ACCEPT
+  /sbin/iptables -A FORWARD --in-interface ${Interface1} -j ACCEPT
 
